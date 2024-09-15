4fc190acb9a34ef66223108670c5a90ec619fa1f
Lasse <lassestu@gmail.com>
2024-09-15 19:49:57 +0200

    added functions

--- .history/next.config_20240914185121.js
+++ .history/next.config_20240914185121.js
@@ -6,6 +6,5 @@ module.exports = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
   // ... other Next.js config options
-};
+}
